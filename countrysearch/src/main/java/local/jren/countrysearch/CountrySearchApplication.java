aadb4361deb1d8f9da99f3ccb26429b801a50da5
Jiayi Ren <jiayiren22@gmail.com>
2020-06-04 13:49:54 -0700

    intelliJ git push test

---
+++ countrysearch/src/main/java/local/jren/countrysearch/CountrySearchApplication.java
@@ -0,0 +1,14 @@
+package local.jren.countrysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrySearchApplication {
+
+    // git push test
+    public static void main(String[] args) {
+        SpringApplication.run(CountrySearchApplication.class, args);
+    }
+
+}
---
+++ countrysearch/src/test/java/local/jren/countrysearch/CountrySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package local.jren.countrysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
